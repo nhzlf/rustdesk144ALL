7114a03acc1de6b2c7836ef7b54c6c8b0b51bca8
nhzlf <nhzlf@163.com>
2026-01-20 10:23:47 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["jetionit.com"];
-pub const RS_PUB_KEY: &str = "8kO3dWUuqmzGMSFlGxAS+RnOztxUCD6jXnWUezHznMs=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["182.92.140.90"];
+pub const RS_PUB_KEY: &str = "u+S8k2OmmL93FQtmXUnkQRKpXJq1RNGBMBPkx5yV4Cw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
